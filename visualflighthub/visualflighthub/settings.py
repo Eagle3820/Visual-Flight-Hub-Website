5517b9d67dfc9f69f3d4f3b255b635e410ab079e
Róbert Örn Ketilsson <38493663+Eagle3820@users.noreply.github.com>
2023-04-15 16:59:12 +0000

    Add files via upload

--- visualflighthub/visualflighthub/settings.py
+++ visualflighthub/visualflighthub/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'welcome.apps.WelcomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- visualflighthub/welcome/views.py
+++ visualflighthub/welcome/views.py
@@ -4,16 +4,22 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-def home(request):
-    if request.user.is_authenticated:
-        # User is logged in, show the main page
-        return render(request, 'home.html')
+# checks if user has seen welcome
+def welcome(request):
+    # User is not logged in, check if they have seen the welcome page before
+    if request.session.get('has_seen_welcome', True):
+        # User has seen the welcome page before, redirect to login page
+        return redirect('home')
     else:
-        # User is not logged in, check if they have seen the welcome page before
-        if request.session.get('has_seen_welcome', False):
-            # User has seen the welcome page before, redirect to login page
-            return redirect('login')
-        else:
-            # User has not seen the welcome page, show it to them and set session variable
-            request.session['has_seen_welcome'] = True
-            return render(request, 'welcome.html')
+        # User has not seen the welcome page, show it to them and set session variable
+        request.session['has_seen_welcome'] = True
+        return render(request, 'welcome.html')
+
+def reset_session(request):
+    if 'has_seen_welcome' in request.session:
+        del request.session['has_seen_welcome']
+    return redirect('home')
+
+# redirect to home page
+def home(request):
+    return render(request, 'home.html')

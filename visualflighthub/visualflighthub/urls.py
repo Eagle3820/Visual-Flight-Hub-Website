0a092c24e5216b717f59cc5e5e34746167c63a20
Róbert Örn Ketilsson <38493663+Eagle3820@users.noreply.github.com>
2023-04-15 17:17:09 +0000

    Add files via upload
    It works now.  But it's very basic.

--- visualflighthub/visualflighthub/urls.py
+++ visualflighthub/visualflighthub/urls.py
@@ -21,7 +21,7 @@ from welcome import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('', views.home, name='home'),
+    path('', views.welcome, name='welcome'),
+    path('home/', views.home, name='home'),
     path('reset_session/', views.reset_session, name='reset_session'),
 ]
